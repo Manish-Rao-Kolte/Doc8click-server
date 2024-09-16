bac415740972e4d0d0c252bc4633865ee1cb4342
Manish-Rao-Kolte <138801683+Manish-Rao-Kolte@users.noreply.github.com>
2024-09-16 19:57:23 +0530

    Update db configuration.

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const config = {
     PORT: process.env.PORT || 5000,
-    MONGO_URI: process.env.MONGO_URI,
+    MONGO_DEV_URI: process.env.MONGO_DEV_URI,
+    MONGO_PROD_URI: process.env.MONGO_PROD_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    DB_NAME: process.env.DB_NAME,
 };
 
 module.exports = config;
--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,18 @@ const config = require('./config');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(config.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        if(config.NODE_ENV === 'development') {
+            mongoose.set('debug', true);
+            await mongoose.connect(config.MONGO_DEV_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+        }else {
+            await mongoose.connect(`${config.MONGO_PROD_URI}?dbName=${config.DB_NAME}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            });
+        }
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
